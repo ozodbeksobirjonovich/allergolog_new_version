96783f4113e61350873652eab57db2660446a1de
jumayevdev <haqnazarjumayev111@gmail.com>
2025-03-04 00:00:07 +0500

    converted to flask app

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/kg")
+def kg():
+    return render_template('kg.html')
+
+@app.route("/ru")
+def ru():
+    return render_template('ru.html')
+
+@app.route("/kz")
+def kz():
+    return render_template('kz.html')
+
+@app.route("/tj")
+def tj():
+    return render_template('tj.html')
+
+@app.route("/tm")
+def tm():
+    return render_template('tm.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8008)
