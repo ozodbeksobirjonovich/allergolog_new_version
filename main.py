ef20aedd252077b5d7e121c80fbe3bdf3b6b99d5
jumayevdev <haqnazarjumayev111@gmail.com>
2025-03-06 13:17:29 +0500

    updated

--- main.py
+++ main.py
@@ -26,5 +26,9 @@ def tj():
 def tm():
     return render_template('tm.html')
 
+@app.route("/ariza")
+def ariza():
+    return render_template('ariza.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8008)
